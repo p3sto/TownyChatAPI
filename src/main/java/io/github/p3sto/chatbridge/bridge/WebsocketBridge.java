141b6d3ab23c6a295b46f0a2748665b3c7fae880
devPesto <pestonotpasta@gmail.com>
2025-06-29 21:49:52 +0200

    Begin WebsocketBridge

---
+++ src/main/java/io/github/p3sto/chatbridge/bridge/WebsocketBridge.java
@@ -0,0 +1,24 @@
+package io.github.p3sto.chatbridge.bridge;
+
+public class WebsocketBridge implements MessageBridge {
+
+	@Override
+	public void connect() {
+
+	}
+
+	@Override
+	public void subscribe(String channel) {
+
+	}
+
+	@Override
+	public void publish(String channel, String message) {
+
+	}
+
+	@Override
+	public void disconnect() {
+
+	}
+}
