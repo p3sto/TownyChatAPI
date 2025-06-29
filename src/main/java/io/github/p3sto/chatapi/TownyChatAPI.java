b349c3c17de9984d8ceffcbc63d30b674e65a5a5
devPesto <pestonotpasta@gmail.com>
2025-06-29 16:05:27 +0200

    Initial commit

---
+++ src/main/java/io/github/p3sto/chatapi/TownyChatAPI.java
@@ -0,0 +1,17 @@
+package io.github.p3sto.chatapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TownyChatAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
